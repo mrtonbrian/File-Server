263afa005e4158fe02ed9dc6b5d35fdef0418b3d
Brian Ton <mrtonbrian@gmail.com>
2017-12-26 19:07:22 -0800

    Improved Fromatting

--- File Transfer Client Upload.py
+++ File Transfer Client Upload.py
@@ -4,9 +4,11 @@ try:
     from tkMessageBox import *
 except:
     from tkinter import *
+
 import socket
 from threading import Thread
 from os import path
+
 class sendThread(Thread):
     def __init__(self,s):
         Thread.__init__(self)
@@ -17,10 +19,12 @@ class sendThread(Thread):
         self.s.send(f.read())
         f.close()
         showinfo("Upload","Upload Complete")
+        
 def open_file():
     global fn,send_butt
     fn = askopenfilename(title='Open File:')
     send_butt.config(state=NORMAL)
+    
 def send_file():
     s = socket.socket()
     host = 'IP Address Here'
@@ -32,11 +36,13 @@ def send_file():
     s.send(path.split(fn)[1])
     g = sendThread(s)
     g.start()
+    
 def main(master):
     global send_butt
     Button(master,text="Browse...",command=open_file).pack()
     send_butt = Button(master,text='Send File',state=DISABLED,command=send_file)
     send_butt.pack()
+    
 if __name__ == '__main__':
     root = Tk()
     main(root)
